5d23ae289492f6113d6ebbcc7049b872bee21ed9
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-04-23 20:35:17 +0800

    Initial Setup for Dependency and Dev

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Onboarding() {
+  return (
+    <div className="bg-base-100 text-primary-content">
+      <h1 className="text-4xl ">Onboarding</h1>
+      <p className="text-lg">localhost:3000/onboarding</p>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,48 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        trails: {
+          "primary": "#4ECAAB",
+          "secondary": "#CCCCCC",
+          "accent": "#FF9183",
+          "neutral": "#FBFBFE",
+          "base-100": "#FBFBFE",
+          "info": "#3E52D3",
+          "success": "#60d273",
+          "warning": "#FFB663",
+          "error": "#CF2020",
+          "indicator-color": "#4ECAAB",
+        }
+      }
+    ],
+  },
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        sm: '0.707rem',
+        base: '1rem',
+        xl: '1.414rem',
+        '2xl': '1.999rem',
+        '3xl': '2.827rem',
+        '4xl': '3.997rem',
+        '5xl': '5.652rem',
+      },
+      fontFamily: {
+        heading: 'Poppins',
+        body: 'Poppins',
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+    require('tailwindcss-animated')
+  ],
 };
 export default config;
