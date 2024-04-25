e8ed67d126218e1a04aa00840a7e6c969ebf4cc8
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-04-25 18:53:10 +0800

    [CHORE] Update page.tsx and hero/index.tsx components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
+import type { Metadata } from "next";
+
 import GlobalLayoutProvider from "@/components/common/GlobalLayoutProvider";
 import LandingHero from "@/components/resources/landing/hero";
-import Image from "next/image";
-import Link from "next/link";
+import HeadTag from "@/components/common/HeadTag";
+
+export const metadata: Metadata = {
+  title: "Trails | Welcome to the future of stock trading!",
+};
 
 export default function Home() {
   return (
     <GlobalLayoutProvider>
+      <HeadTag description="Create Your Free Account Now!" icon="information-line" containerStyle="bg-info text-info-content"/>
       <LandingHero />
     </GlobalLayoutProvider>
   );
--- src/components/resources/landing/hero/index.tsx
+++ src/components/resources/landing/hero/index.tsx
@@ -1,9 +1,16 @@
 'use client'
 
+import ContentLayoutProvider from "@/components/common/ContentLayoutProvider"
+
 export default function LandingHero() {
   return (
-    <div className="bg-base-100 text-primary-content">
-      This is the hero of the landing page
+    <div className="bg-base-100 h-full bg-gradient-to-b from-transparent via-transparent to-primary text-primary-content">
+      <ContentLayoutProvider>
+        <div className="flex flex-col items-start justify-center h-full w-2/3 bg-">
+          <h1 className="text-6xl font-bold w-full">Your Gateway to Smarter Trading</h1>
+          <p className="font-medium">A place to find all the resources you need</p>
+        </div>
+      </ContentLayoutProvider>
     </div>
   )
 }
