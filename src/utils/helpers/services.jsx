559300f98dedf3f3cd2d876217b288dfe83b8a35
Paulene Pallan <73598647+paulenepal@users.noreply.github.com>
2024-04-29 00:12:24 +0800

    chore: services [to be moved to constants folder]

---
+++ src/utils/helpers/services.jsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export const API_URL = 'http://localhost:3001'
+
+export const GetUserInfo = () => {
+  return JSON.parse(sessionStorage.getItem('user'));
+};
+  
+export const GetToken = () => {
+  return sessionStorage.getItem('token');
+};
+
+export const GetIEXStocks = async () => {
+  try {
+    const token = GetToken();
+    const response = axios.get(`${API_URL}/watchlist`, 
+  {
+    headers: {
+      'authorization': `${token}`,
+      'Accept': 'application/json'
+    },
+  });
+    return response;
+  } catch (error) {
+    return error.error_message;
+  }
+}
+
+export const GetIEXStock = (symbol) => {
+  try {
+    const response = axios.get(`${API_URL}/watchlist/${symbol}`, 
+  {
+    headers: {
+      Authorization: `${GetToken()}`,
+    },
+  });
+    return response.data;
+  } catch (error) {
+    return error.error_message;
+  }
+}
