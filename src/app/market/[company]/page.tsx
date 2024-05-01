b598f11af2fa371be44682e14fb33f8512ac0d15
Paulene Pallan <73598647+paulenepal@users.noreply.github.com>
2024-05-01 12:52:53 +0800

    feat/wip: added charts [working copy]

--- src/app/market/[company]/page.tsx
+++ src/app/market/[company]/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 
-import React, { useState, useEffect, use } from "react";
+import React, { useState, useEffect } from "react";
 
-import { StockData } from "@/utils/types/stocktypes";
+import MainContentLayout from "@/components/providers/MainContentLayout"
 import { GetIEXStock, GetToken } from "@/utils/helpers/services";
-
+import { capitalize } from "@/utils/helpers/name-formatter";
+import Stonk from "@/components/resources/market/CandleStick";
+import AreaChart from "@/components/resources/market/AreaChart";
+import AreaChartNoGrid from "@/components/resources/market/AreaChartNoGrid";
 
 export default function Page({ params }: { params: {company: string}}) {
-  const [stock, setStock] = useState<StockData | null>(null);
+  const [stock, setStock] = useState([]);
+  const stockName = capitalize(params.company)
 
   useEffect(() => {
     const fetchStock = async () => {
@@ -15,9 +19,8 @@ export default function Page({ params }: { params: {company: string}}) {
         const token = GetToken();
       
         if (token) {
-          const stock = await GetIEXStock(params.company);
-          setStock(stock.data);
-          console.log(stock.data);
+          const response = await GetIEXStock(params.company);
+          setStock(response.data);
         } else {
           console.error("User not logged in");
         }
@@ -28,13 +31,48 @@ export default function Page({ params }: { params: {company: string}}) {
     fetchStock();
   }, [params.company]);
 
-  return (
-    <div>
-      <h1>This Page is {params.company}</h1>
+  console.log(stock);
+
+  const formattedChartData = stock.historical_prices ? stock.historical_prices.map((item) => ({
+    date: item.date,
+    open: item.open,
+    low: item.low,
+    high: item.high,
+    close: item.close,
+    volume: item.volume
+  })) : [];
   
-        {/* hey: {stock.latest_price} */}
+
+  console.log(formattedChartData);
+
+  return (
+    <>
+    <MainContentLayout>
+    <div>
+      <h1>This Page is {stockName}</h1>
+  
+      {stockName} latest price: {stock.latest_price}
+      <p>News Headline: {stock && stock.news && stock.news.headline}</p>
+      <p>{stock && stock.historical_prices && stock.historical_prices[0].close }</p>
+      
+      <Stonk
+        chartData={formattedChartData}
+      />
+      <br />
+
+      <AreaChart
+        chartData={formattedChartData}
+        />
+
+        <br />
+
+      <AreaChartNoGrid
+        chartData={formattedChartData}
+      />
 
     </div>
+    </MainContentLayout>
+    </>
   );
 };
 
