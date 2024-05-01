eaac9f587907517382496383f38e38089ca78d2e
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-05-01 08:52:46 +0800

    [CHORE] refactor layout providers

--- src/components/providers/GlobalLayoutProvider/index.tsx
+++ src/components/providers/GlobalLayoutProvider/index.tsx
@@ -2,9 +2,12 @@
 
 import { ReactNode } from "react"
 
+import SideNavigation from "@/components/common/SideNavigation"
+
 export default function GlobalLayoutProvider({ children } : { children: ReactNode}) {
   return (
     <div className="w-full h-full">
+      <SideNavigation type={'user'}/>
       {children}
     </div>
   )
--- src/components/providers/MainContentLayout/index.tsx
+++ src/components/providers/MainContentLayout/index.tsx
@@ -7,7 +7,6 @@ import SideNavigation from "@/components/common/SideNavigation"
 export default function MainContentLayout({ children } : { children: ReactNode}) {
   return (
     <>
-      <SideNavigation type={'user'}/>
       <div className="h-full w-auto max-md:pl-[5rem] pl-[18rem] ml-14 py-4 pr-4 flex flex-col gap-2">
         {children}
       </div>
