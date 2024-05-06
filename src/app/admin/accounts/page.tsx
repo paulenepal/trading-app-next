646ad65f1b3b534fd34602870b0729ec255710fc
Paulene Pallan <73598647+paulenepal@users.noreply.github.com>
2024-05-07 01:02:54 +0800

    feat: [admin] display all user accounts

--- src/app/admin/accounts/page.tsx
+++ src/app/admin/accounts/page.tsx
@@ -1,14 +1,42 @@
 'use client'
 
+import React, { useEffect, useState } from 'react'
 import MainContentLayout from "@/components/providers/MainContentLayout"
 import SharedLayoutProvider from "@/components/providers/SharedLayoutProvider"
+import { GetAllTraders, GetToken } from '@/utils/helpers/services'
+import Traders from '@/components/common/Lists/Admin/Traders'
 
 export default function Accounts() {
+  const [traders, setTraders] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTraders = async () => {
+    try {
+      const token = GetToken();
+      if (token) {
+        const response = await GetAllTraders(token);
+        setTraders(response.data);
+        console.log(response.data);
+        setLoading(false);
+      } else {
+        console.error('Failed to fetch active users.')
+      }
+    } catch (error) {
+      console.error("Error", error)
+      setLoading(false);
+    }
+  };
+
+  useEffect (() => {
+    fetchTraders();
+  }, [])
+
   return (
     <SharedLayoutProvider>
       <MainContentLayout>
-        Admin Accounts
+      <h1 className="text-2xl font-bold text-gray-900">User Accounts</h1>
+      <Traders traders={traders} loading={loading} buttonType={'Details'}/>
       </MainContentLayout>
     </SharedLayoutProvider>
   )
-}
+} 
