64668f38f0f7ffda6139a67058f58fdb25ad3551
Paulene Pallan <73598647+paulenepal@users.noreply.github.com>
2024-05-07 01:03:30 +0800

    feat: [admin] display all pending accounts [wip: modal to to confim approval]

--- src/app/admin/pending/page.tsx
+++ src/app/admin/pending/page.tsx
@@ -1,14 +1,42 @@
 'use client'
 
-import AdminLayoutProvider from "@/components/providers/AdminLayoutProvider"
+import React, { useEffect, useState } from 'react'
 import MainContentLayout from "@/components/providers/MainContentLayout"
+import SharedLayoutProvider from "@/components/providers/SharedLayoutProvider"
+import { GetPendingTraders, GetToken } from '@/utils/helpers/services'
+import ActiveTraders from '@/components/common/Lists/Admin/Traders'
 
-export default function PendingAccounts() {
+export default function Accounts() {
+  const [traders, setTraders] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTraders = async () => {
+    try {
+      const token = GetToken();
+      if (token) {
+        const response = await GetPendingTraders(token);
+        setTraders(response.data);
+        console.log(response.data);
+        setLoading(false);
+      } else {
+        console.error('Failed to fetch active users.')
+      }
+    } catch (error) {
+      console.error("Error", error)
+      setLoading(false);
+    }
+  };
+
+  useEffect (() => {
+    fetchTraders();
+  }, [])
+
   return (
-    <AdminLayoutProvider>
+    <SharedLayoutProvider>
       <MainContentLayout>
-        Admin Approvals
+      <h1 className="text-2xl font-bold text-gray-900">Pending Users</h1>
+      <ActiveTraders traders={traders} loading={loading} buttonType={'Approve'} />
       </MainContentLayout>
-    </AdminLayoutProvider>
+    </SharedLayoutProvider>
   )
-}
+} 
