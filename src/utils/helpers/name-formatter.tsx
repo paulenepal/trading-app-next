538f299b3def73698a64bfabbd47e8fd7d573f7b
Paulene Pallan <73598647+paulenepal@users.noreply.github.com>
2024-05-07 01:05:25 +0800

    feat: added firstLetter getter function for user avatar placeholder

--- src/utils/helpers/name-formatter.tsx
+++ src/utils/helpers/name-formatter.tsx
@@ -1,3 +1,11 @@
 export function capitalize(str: string): string {
-    return str.toUpperCase();
+  return str.toUpperCase();
+}
+
+export function firstLetter(name: string): string {
+  if (typeof name === 'string' && name.length > 0) {
+    return name.charAt(0).toUpperCase();
+  } else {
+    return '-';
+  }
 }
