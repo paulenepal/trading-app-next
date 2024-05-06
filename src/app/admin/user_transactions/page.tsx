851295722bacb87811d98aadb04a523d959f9227
Paulene Pallan <73598647+paulenepal@users.noreply.github.com>
2024-05-07 01:03:49 +0800

    feat: display all user transactions

--- src/app/admin/user_transactions/page.tsx
+++ src/app/admin/user_transactions/page.tsx
@@ -1,14 +1,42 @@
 'use client'
 
-import AdminLayoutProvider from "@/components/providers/AdminLayoutProvider"
+import React, { useEffect, useState } from 'react'
 import MainContentLayout from "@/components/providers/MainContentLayout"
+import SharedLayoutProvider from "@/components/providers/SharedLayoutProvider"
+import { GetAllTransactions, GetToken, GetUserInfo } from '@/utils/helpers/services'
+import UserTransactions from '@/components/common/Lists/Admin/UserTransactions'
 
-export default function UserTransactions() {
+export default function Accounts() {
+  const [userTransactions, setUserTransactions] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchUserTransactions = async () => {
+    try {
+      const token = GetToken();
+      if (token) {
+        const response = await GetAllTransactions(token);
+        setUserTransactions(response.data);
+        console.log(response.data);
+        setLoading(false);
+      } else {
+        console.error('Failed to fetch active users.')
+      }
+    } catch (error) {
+      console.error("Error", error)
+      setLoading(false);
+    }
+  };
+
+  useEffect (() => {
+    fetchUserTransactions();
+  }, [])
+
   return (
-    <AdminLayoutProvider>
+    <SharedLayoutProvider>
       <MainContentLayout>
-        All user transactions
+      <h1 className="text-2xl font-bold text-gray-900">User Transactions</h1>
+      <UserTransactions transactions={userTransactions} loading={loading} />
       </MainContentLayout>
-    </AdminLayoutProvider>
+    </SharedLayoutProvider>
   )
-}
+} 
