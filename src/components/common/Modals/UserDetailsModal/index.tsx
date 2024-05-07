573d0a7779a81bdf4854066cf2b23dd1b3accce8
Paulene Pallan <73598647+paulenepal@users.noreply.github.com>
2024-05-07 20:56:10 +0800

    feat: modal to view user details

---
+++ src/components/common/Modals/UserDetailsModal/index.tsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { GetToken, GetUserDetails } from "@/utils/helpers/services";
+import { closeModal } from "@/utils/helpers/modalcontrols";
+import { formatBirthdate } from "@/utils/helpers/date-formatter";
+
+import Icon from "../../icon";
+
+export default function UserDetailsModal({ userId } : {userId: string}) {
+  const [userDetails, setUserDetails] = useState<any>(null);
+
+  const fetchUserDetails = async () => {
+    try {
+      const token = GetToken();
+      if (token) {
+        const response = await GetUserDetails(userId);
+        setUserDetails(response.data.data)
+      } else {
+        console.error('User not logged in');
+      }
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+      return error;
+    }
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, [userId]);
+
+  return (
+    <>
+    <dialog id={`${userDetails?.id}_modal`} className="modal modal-bottom sm:modal-middle">
+      <div className="modal-box h-fit flex flex-col justify-between">
+        <div>
+          <h3 className="font-bold text-lg flex flex-row gap-2">
+            <Icon iconName="user-5-fill" />
+            User ID: TX00{userDetails?.id}
+          </h3>
+          <table className="table mt-6">
+            <tbody>
+              <tr>
+                <td className="font-semibold">Full Name</td>
+                <td>{userDetails?.first_name} {userDetails?.last_name}</td>
+              </tr>
+              <tr>
+                <td className="font-semibold">Birthday</td>
+                <td>{userDetails && formatBirthdate(userDetails.birthday)}</td>
+              </tr>
+              <tr>
+                <td className="font-semibold">Email Address</td>
+                <td>{userDetails?.email}</td>
+              </tr>
+              <tr>
+                <td className="font-semibold">Username</td>
+                <td>{userDetails?.username}</td>
+              </tr>
+              <tr>
+                <td className="font-semibold">Status</td>
+                <td>
+                  {userDetails?.role === 'pending_trader' || userDetails?.role === null
+                    ? 'Pending Trader'
+                    : userDetails?.role === 'trader'
+                    ? 'Approved Trader'
+                    : ''}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          
+        </div>
+        <div className="modal-action">
+          <form method="dialog">
+            <button className="btn" onClick={() => {closeModal(`${userDetails?.id}_modal`)}}>
+              Close
+            </button>
+          </form>
+        </div>
+      </div>
+    </dialog>
+    </>
+  );
+};
