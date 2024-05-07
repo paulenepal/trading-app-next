8ee5ddd20155e8d1427f9c90c6fe99fef79c7525
Paulene Pallan <73598647+paulenepal@users.noreply.github.com>
2024-05-07 22:05:23 +0800

    feat: mapped each user to get user data for each transaction

--- src/components/common/Lists/Admin/UserTransactions/index.tsx
+++ src/components/common/Lists/Admin/UserTransactions/index.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { formatToDollar } from "@/utils/helpers/currency-formatter";
+import { formatTimestamp } from "@/utils/helpers/date-formatter";
+import { firstLetter } from "@/utils/helpers/name-formatter";
+import { FormatTransactionTypeAdmin } from "@/utils/helpers/datatype-formatter";
+import { GetToken, GetUserDetails } from "@/utils/helpers/services";
 
 export default function UserTransactions ( { transactions, loading } : { transactions: any, loading: boolean }) {
+  const [userDetails, setUserDetails] = useState({});
+
+  useEffect(() => {
+    const fetchUserDetails = async (userId) => {
+      try {
+        const token = GetToken();
+        if (token) {
+          const response = await GetUserDetails(userId);
+          setUserDetails((prevUserDetails) => ({
+            ...prevUserDetails, [userId]: response.data.data,
+          }));
+        } else {
+          console.error('User not logged in');
+        }
+
+      } catch (error) {
+        console.error('Error fetching user details:', error)
+      }
+    }
+    if (transactions && transactions.data) {
+      transactions.data.forEach((transaction: any) => {
+        fetchUserDetails(transaction.user_id);
+      });
+    }
+  }, [transactions]);
 
   return (
     <>
@@ -13,8 +42,8 @@ export default function UserTransactions ( { transactions, loading } : { transac
               <th className="text-center">Transaction ID</th>
               <th>User ID</th>
               <th>Transaction Type</th>
-              <th>Amount</th>
-              <th></th>
+              <th>Transaction Amount</th>
+              <th>Transaction Date</th>
             </tr>
           </thead>
           <tbody>
@@ -26,16 +55,16 @@ export default function UserTransactions ( { transactions, loading } : { transac
                   <div className="flex justify-center items-center gap-4">
                     <div className="avatar online placeholder">
                       <div className="bg-secondary text-neutral-content rounded-full w-16">
-                        <span className="text-xl">UID</span>
+                        <span className="text-xl">{firstLetter(userDetails[transaction.user_id]?.first_name)}{firstLetter(userDetails[transaction.user_id]?.last_name)}</span>
                       </div>
                     </div>
                     <div>
-                      <div className="font-bold">TX00{transaction.user_id}</div>
-                      {/* <div className="text-sm opacity-50">@{transaction.username}</div> */}
+                      <div className="font-bold text-left">{userDetails[transaction.user_id]?.first_name || `TX00${transaction.user_id}`} {userDetails[transaction.user_id]?.last_name || `TX00${transaction.user_id}`}</div>
+                      <div className="text-sm opacity-50 text-left">@{userDetails[transaction.user_id]?.username || `TX00${transaction.user_id}`}</div>
+                      <div className="text-sm opacity-50 text-left">UID: TX00{transaction.user_id}</div>
                     </div>
                   </div>
                 </td>
-            
                 <td className="font-bold">
                   <span className="badge badge-ghost badge-xl" >
                   {(() => {
@@ -68,17 +97,25 @@ export default function UserTransactions ( { transactions, loading } : { transac
                   </span>
                 </td>
                 <td className="font-bold">
-                  USD {formatToDollar(transaction.price)}
+                  $ {(parseFloat(transaction.price) * transaction.quantity).toFixed(2)}
+                  <div className="font-normal text-sm opacity-50">
+                    {transaction.symbol}
+                    {transaction.transaction_type === 'share_buy' || transaction.transaction_type === 'share_sell' ? (
+                      <> Units: ({transaction.quantity})
+                      <p>Price: {formatToDollar(transaction.price)} </p>
+                       </>
+                    ) : null}
+                  </div>
                 </td>
-                <th>
-                  <button className="btn btn-ghost btn-xs"> Details </button>
-                </th>
+                <td>
+                  {formatTimestamp(transaction.created_at)}
+                </td>
               </tr>
             ))
           ) : transactions == undefined && !loading? (
             <tr>
                 <td colSpan={7} className="text-center">
-                  No Transactions F
+                  No Transactions Found.
                 </td>
               </tr>
             ) : (
